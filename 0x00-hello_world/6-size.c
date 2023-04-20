98095d951c64e7ae0559735d4b327114e3de8c48
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-20 13:20:51 -0700

    Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %d\(s\)", sizeof(char));
-	printf("Size of an int : %d\(s\)", sizeof(int));
-	printf("Size of a long int: %d\(s\)", sizeof(long int));
-	printf("Size of a long long int: %d\(s\)", sizeof(long long int));
-	printf("Size of a float: %d\(s\)", sizeof(float));
+	printf("Size of a char: %d\(s\)\n", sizeof(char));
+	printf("Size of an int : %d\(s\)\n", sizeof(int));
+	printf("Size of a long int: %d\(s\)\n", sizeof(long int));
+	printf("Size of a long long int: %d\(s\)\n", sizeof(long long int));
+	printf("Size of a float: %d\(s\)\n", sizeof(float));
 
 	return (0);
 }
