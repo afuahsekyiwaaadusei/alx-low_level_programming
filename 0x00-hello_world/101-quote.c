3e8b7cd8201362d59f311b69a7115af316e00dcf
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-20 16:50:38 -0700

    Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Zero if successfull
+ */
+
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, err, strnlen(err, sizeof(err)));
+
+	return (0);
+}
