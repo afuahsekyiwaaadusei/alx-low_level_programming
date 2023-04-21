da647bf97783d4d9b8f877625c8db651dd682e72
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-21 11:40:29 -0700

    Task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Zero if successfull
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
