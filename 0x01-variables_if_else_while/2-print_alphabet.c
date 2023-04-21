f86231959454d05a71302bb775bdf8a907133ec0
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-21 10:26:18 -0700

    Task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Zero if successfull
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+
+	}
+
+	return (0);
+}
