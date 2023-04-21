dd68d101cee44f9f303dc9e3fe7539e676df309d
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-21 11:17:14 -0700

    Task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main -Entry Point
+ *
+ *Return: Zero when successfull
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e')
+		{
+			ch++;
+		}
+		else if (ch == 'q')
+		{
+			ch++;
+		}
+		else
+		{
+			putchar(ch);
+			ch++
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
