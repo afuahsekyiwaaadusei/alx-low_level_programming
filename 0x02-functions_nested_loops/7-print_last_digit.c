09648978cda0388389f4da6ed6a79f029d8406c7
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-26 10:50:54 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit of a base 10 integer
+ *@n: the integer
+ *
+ *Return: the last digit of the integer
+ */
+
+int print_last_digit(int n)
+{
+	int ldigit = n % 10;
+
+	return (ldigit);
+}
