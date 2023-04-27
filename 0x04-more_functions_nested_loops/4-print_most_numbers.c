c36c60b3fd48374d8a5a964e168d871c751dd687
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-27 14:47:13 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_most_number - prints 0 to 9 excluding 2 and 4 followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	char ch = 48;
+
+	while (ch <= 57)
+	{
+		if (ch == 50 || ch == 52)
+		{
+			ch++;
+		}
+		else
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+}
