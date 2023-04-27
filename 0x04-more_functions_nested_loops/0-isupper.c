3f6b9beebf22c87bee6e3cabd6f91096d3ba5111
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-04-27 12:37:36 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,7 +10,19 @@
 int _isupper(int c)
 {
 	if (c >= 65)
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(32);
+		_putchar(49);
 		return (1);
+	}
 	else
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(32);
+		_putchar(48);
 		return (0);
+	}
 }
