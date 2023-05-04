61ff4d198f09956b86fe610f536c4d36a85d477e
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-04 13:04:11 -0700

    Task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1: pointer to first string
+ *@s2: pointer to second string
+ *
+ *Return:  an integer less than, equal to, or greater than zero if s1 is found
+ *respectively, to be less than, to match, or be greater than s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+	{
+		if (*(s1 + i) < *(s2 + i))
+		{
+			return ((*(s1 + i)) - (*(s2 + i)));
+		}
+		else if (*(s1 + i) > *(s2 + i))
+		{
+			return ((*(s1 + i)) - (*(s2 + i)));
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+	return (0);
+}
