81ad2464d2b3e3f3418d35f47dd2ce147b9a2534
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-05 13:24:57 -0700

    Task 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_number - a function that prints an integer
+ *@n: integer to be printed
+ */
+void print_number(int n)
+{
+
+	int num = 0;
+
+	int i = 0;
+
+	while (num > 0)
+	{
+		
+	}
+
+
+	char str[] = (char) n;
+	
+	_putchar(str);
+}
