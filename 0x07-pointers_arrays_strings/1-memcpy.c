34301acfbb99ac0591d238ef3a5594ef3d2f911d
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-08 09:23:57 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies n bytes from memory area src to memory area dest
+ *@dest: destination memory area
+ *@src: source memory area
+ *@n: number of bytes to be copied
+ *
+ *Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
