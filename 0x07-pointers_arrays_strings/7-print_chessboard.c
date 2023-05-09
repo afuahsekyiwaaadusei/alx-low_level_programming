e6c21d7ec55e92b9ff5214657f831c3e128e0030
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-09 11:00:48 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_chessboard -  a function that prints the chessboard.
+ *@a: rows of the board
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	while (i < 8)
+	{
+		int j = 0;
+
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
