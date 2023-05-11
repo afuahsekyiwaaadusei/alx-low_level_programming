8b40d8846d43e0cc441f747c07eae16be4ccf6ec
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-11 10:49:46 -0700

    Task 1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number.
+ *@n: number
+ *
+ *Return: natural square root of a number.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (count);
+	}
+
+	return (_sqrt_recursion(n-(i+2))
+
+}
