8b6384751cd8309169bf5c5d523fe0e4e5b9550a
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-11 15:55:45 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,50 +1,33 @@
 #include "main.h"
 
-/**
- *_odd_num - returns odd numbers.
- *@o:pointer number
- *
- *Return: an odd number
- */
-
-int _odd_num(int *o, int n)
-{
-	if (n == 0)
-	{
-		return (*o); 
-	}
-
-	if (n < 0)
-	{
-		return (-1);
-	}
-		
-
-	if (*o % 2 != 0)
-	{
-		n = n - *o;
-		*o = *o + 1;
-	}
-	else
-	{
-		*o = *o + 1;
-	}
-
-	return (_odd_num(o,n));
-	
-}
-
+int actual_sqrt_recursion(int n, int i);
 
 /**
- *_sqrt_recursion - returns the natural square root of a number.
- *@n: number
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- *Return: natural square root of a number.
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
-	int odd = 0;
-	int *ptr = &odd;
-	int result = _odd_num(ptr,n);
-	return (result > 0 ? result / 2 : result);
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
