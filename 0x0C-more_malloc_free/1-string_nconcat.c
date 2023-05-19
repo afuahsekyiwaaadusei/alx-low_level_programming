a2ce8e83d3c541460e0f3600ecb92a8dbde6b711
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-19 15:54:11 -0700

    Task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 
-
 /**
  *get_length - returns the length of a string.
  *@str: string.
@@ -12,8 +11,8 @@
 
 int get_length(char *str)
 {
-	int i = 0;
-	int len = 0;
+	unsigned int i = 0;
+	unsigned int len = 0;
 
 	while (str[i] != '\0')
 	{
@@ -39,42 +38,43 @@ int get_length(char *str)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2;
-	unsigned int i = 0, j = 0;
+	unsigned int len1, len2, size, i = 0, j = 0;
 	char *new_mem;
-
 
 	if (s1 == NULL)
 	{
-		s1 = '\0';
-		len1 = 1;
+		s1 = "";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
-		s2 = '\0';
-		len2 = 1;
+		s2 = "";
 	}
-	else
-	{
-		len1 = get_length(s1);
-		len2 = get_length(s2);
-	}
-	new_mem = malloc(sizeof(*new_mem) * (len1 + len2 + 1));
+	len2 = get_length(s2);
+	len1 = get_length(s1);
+
+	if (n >= len2)
+		n = len2;
+	size = len1 + n;
+
+	new_mem = malloc(sizeof(*new_mem) * size + 1);
+
 	if (new_mem == NULL)
 		return (NULL);
+
 	while (i < len1)
 	{
 		new_mem[i] = s1[i];
 		i++;
 	}
-	if (n >= len2)
-		n = len2;
+
 	while (j < n)
 	{
 		new_mem[i] = s2[j];
 		i++;
 		j++;
 	}
+
 	new_mem[i] = '\0';
+
 	return (new_mem);
 }
