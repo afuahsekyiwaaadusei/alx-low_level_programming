b0acd28276231c1fc729496af5e0cba1e0eca7f8
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-23 12:48:18 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *Dog;
+	unsigned int nl, ol, i;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
+
+
+	/*struct dog *Dog;
 
 	Dog = malloc(sizeof(struct dog));
 	if (!Dog)
@@ -23,5 +58,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	Dog->name = name;
 	Dog->age = age;
 	Dog->owner = owner;
-	return (Dog);
+	return (Dog);*/
 }
