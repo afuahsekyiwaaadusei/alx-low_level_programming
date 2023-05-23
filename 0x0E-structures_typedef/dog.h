02ed0770811f22d961e11740385ba2dda03c227d
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-23 10:31:30 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - struct about a puppy.
+ *@name: name of puppy.
+ *@age: age of puppy.
+ *@owner: owner.
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
