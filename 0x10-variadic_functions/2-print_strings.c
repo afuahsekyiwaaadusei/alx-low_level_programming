c2b5c3dc783eb03a532ff057ab91be7bb799dea2
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-25 11:09:43 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,10 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (str == NULL)
 			printf("(nil)");
-		if (separator != NULL && i != n - 1)
-			printf("%s%s", str, separator);
 		else
 			printf("%s", str);
+		if ((separator != NULL) && (i != n - 1))
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
