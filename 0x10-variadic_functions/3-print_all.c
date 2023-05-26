02e5d3f5bb0d09ec35e782f1267840c4dd51e3c2
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-26 09:04:28 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,34 +16,8 @@ void print_all(const char * const format, ...)
 	char *str;
 
 	va_start(args, format);
-	while (ptr[i] != '\0' && ptr[i + 1] != '\0')
-	{
-		switch (ptr[i])
-		{
-		case 'c':
-			printf("%c, ", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d, ", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f, ", va_arg(args, double));
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s, ", str);
-			break;
-		default:
-			break;
-		}
-		i++;
-	}
-	while (ptr[i] != '\0' && ptr[i + 1] == '\0')
+	
+	while (ptr != NULL && ptr[i] != '\0')
 	{
 		switch (ptr[i])
 		{
@@ -68,6 +42,10 @@ void print_all(const char * const format, ...)
 		default:
 			break;
 		}
+
+		if ((ptr[i] == 'c' || ptr[i] == 'i' || ptr[i] == 'f' ||
+		ptr[i] == 's') && ptr[(i + 1)] != '\0')
+			printf(", ");
 		i++;
 	}
 	printf("\n");
