9259e2fd74ce5b29683fdcb74c96a4c729d013fa
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-05-30 11:46:24 -0700

    bug fix

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ *get_op_func - selects the correct function to perform
+ *the operation asked by the user.
+ *@s: - the operator passed as argument to the program.
+ *
+ *Return: a pointer to the function that corresponds
+ *to the operator passed as arguement.
+ *the function returned has parameters (int, int) and will itself return an int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (i < 6)
+	{
+		op_t operator = ops[i];
+
+		if (operator.op == s)
+			return (operator.f);
+		i++;
+	}
+	return (NULL);
+
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ *main - performs simple operations
+ *@argc: count of arguements passed to the program.
+ *@argv: array of pointers to the arguement passed to the program.
+ *
+ *Return: zero on success
+ */
+
+int main(int argc, char* argv[])
+{
+	int a, b;
+	int (*arith)(int, int);
+
+	if (argc > 4)
+	{
+		printf("Ergc is > 4 Error\n");
+		exit(98);
+	}
+
+	if ((argv[3] == 0) && (argv[2] == '/' || argv[2] == '%'))
+	{
+		printf("division or modulus by zero Error\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	arith = get_op_func(argv[2]);
+
+	if (!arith)
+	{
+		printf("operator invalid Error\n");
+		exit(99);
+	}
+	printf("%d\n", arith(a, b));
+
+	return (0 );
+}
