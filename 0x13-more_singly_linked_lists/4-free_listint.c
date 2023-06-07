036542cb3e95f36646d95dc3dc2c9eb08e1f0e8e
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-06-06 17:32:45 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *frre_listint - a function that frees a listint_t list.
+ *@head: ths start of the list.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
