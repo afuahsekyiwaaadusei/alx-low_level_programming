4ab77bf438d1faebc0681a80813e0217ee5344de
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2023-06-16 09:52:37 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rd = read(fd, buf, letters);
 	if (rd == -1)
 		return (0);
-	wr = write(STDOUT_FILENO, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
 	if (wr == -1 || wr < count)
 		return (0);
 	close(fd);
