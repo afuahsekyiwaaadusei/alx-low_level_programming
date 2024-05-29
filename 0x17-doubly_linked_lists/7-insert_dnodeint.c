e89b20d36c19419db6d9389544c0e429a858dc11
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-29 09:39:54 -0700

    task 7 account for insertion at the begining, middle and end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,29 +15,42 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h, *new_node;
 	unsigned int count = 0;
 
-	while (current->next != NULL)
-	{
-		if (count == idx)
-			break;
-		count++;
-		current = current->next;
-	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (current->next == NULL)
+	if (current == NULL)
 	{
-		new_node->prev = current;
+		new_node->prev = NULL;
 		new_node->next = NULL;
-		current->next = new_node;
 	}
 	else
 	{
-		new_node->prev = current->prev;
-		new_node->next = current;
-		if (current->prev != NULL)
+		if (idx == 0)
+		{
+			new_node->prev = NULL;
+			new_node->next = current;
+			current->prev = new_node;
+		}
+		while (current->next != NULL)
+		{
+			if (count == idx)
+				break;
+			count++;
+			current = current->next;
+		}
+		if (current->next == NULL)
+		{
+			new_node->prev = current;
+			new_node->next = NULL;
+			current->next = new_node;
+		}
+		else
+		{
+			new_node->prev = current->prev;
+			new_node->next = current;
 			current->prev->next = new_node;
+		}
 	}
 	return (new_node);
 }
