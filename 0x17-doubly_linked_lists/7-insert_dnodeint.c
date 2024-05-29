9081774f4544b3b360068238435802a38cccb644
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-29 07:49:11 -0700

    Account for one node list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,8 +29,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = current;
 	if (current != NULL)
+	{
 		new_node->prev = current->prev;
 		new_node->n = n;
 		current->prev->next = new_node;
+	}
 	return (new_node);
 }
