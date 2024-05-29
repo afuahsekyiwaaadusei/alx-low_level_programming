d2695dbc77dab778dc9257f0e6c2e1e23b1fca6d
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-29 07:47:31 -0700

    Account for one node list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,12 +22,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		current = current->next;
 	}
+	
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->prev = current->prev;
+	new_node->prev = NULL;
 	new_node->next = current;
-	new_node->n = n;
-	current->prev->next = new_node;
+	if (current != NULL)
+		new_node->prev = current->prev;
+		new_node->n = n;
+		current->prev->next = new_node;
 	return (new_node);
 }
